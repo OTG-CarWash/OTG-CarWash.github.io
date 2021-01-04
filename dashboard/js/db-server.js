6eae8f7c9a66cf6dbc2e6446abb3b2910046a0dc
Sanatan Shrivastava <sanatan.cseiiitk@gmail.com>
2021-01-04 10:19:45 +0530

    added assign the worker feature

--- dashboard/js/db-server.js
+++ dashboard/js/db-server.js
@@ -1,49 +1,36 @@
 var db = firebase.firestore();
-// var currentDate = Date.now();
-// const table = document.getElementById('data');
-
-// var dates = new Set();
-// var button = document.getElementById('current_date');
-// db.collection("portal").get().then(function (querySnapshot) {
-//     querySnapshot.forEach(function (doc) {
-//         currentDate = doc.data().timestamp.toDate().toLocaleDateString();
-//         if (!dates.has(currentDate)) {
-//             dates.add(currentDate);
-//             button.innerHTML += "<button onclick='' class='btn btn primary'>" + currentDate + "</button> <br/>";
-//         }
-//     });
-// }).catch(function (error) {
-//     console.log("Error getting document:", error);
-// });
-
 var query = db.collection("portal");
 const table = document.getElementById('data');
 
-// const orderPlaced = document.getElementById('orderPlaced');
+
 query.onSnapshot(function (querySnapshot) {
     querySnapshot.docChanges().forEach(function (change) {
-        var orderId = change.doc.data().orderId;
-        var amount = change.doc.data().amount;
-        var contact = change.doc.data().contact;
-        var username = change.doc.data().name;
-        var timestamp = change.doc.data().timestamp;
-
+    
         if (change.type === "added") {
-            table.innerHTML += "<tr><td>" + orderId + "</td>" +
+            var orderId = change.doc.data().orderId;
+            var docId = change.doc.data().docId;
+            var amount = change.doc.data().amount;
+            var contact = change.doc.data().contact;
+            var username = change.doc.data().name;
+            var timestamp = change.doc.data().timestamp;
+            var assignedTo = change.doc.data().assignTo;
+
+            table.innerHTML += "<tr><td id='order-id'>" + orderId + "</td>" +
                 "<td>" + username + "</td>" +
                 "<td>" + contact + "</td>" +
                 "<td>" + amount / 100 + "</td>" +
                 "<td>" + timestamp.toDate().toLocaleDateString() + "</td>" +
-                `<td><button class="btn btn-dark" id='myBtn' onclick="on('${orderId}')">view</button></td></tr>`;
+                `<td><button class="btn btn-dark" id='myBtn' onclick="on('${docId}')">view & assign</button></td>` +
+                `<td id="assignedCell">${assignedTo}</td></tr>`;
         }
+        
     });
 });
-
 
 function on(userId) {
     console.log(userId);
     // Get the modal
-    var modal = document.getElementById("myModal");
+    var modal = document.getElementById("receipt-modal");
 
     // Get the button that opens the modal
     var btn = document.getElementById("myBtn");
@@ -53,12 +40,24 @@ function on(userId) {
 
     // When the user clicks the button, open the modal 
     modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
     
-    db.collection("portal").where("orderId", "==", userId)
+    db.collection("portal").where("docId", "==", userId)
         .get()
         .then(function (querySnapshot) {
             querySnapshot.forEach(function (doc) {
-                document.getElementById('modal-orderid').innerHTML = "Invoice details - Order #" + userId;
+                document.getElementById('modal-orderid').innerHTML = "Invoice details - Order #" + doc.data().orderId;
                 document.getElementById('order-date').innerHTML = doc.data().timestamp.toDate().toLocaleDateString();
                 
                 if (doc.data().success === true) {
@@ -70,21 +69,25 @@ function on(userId) {
                 }
 
                 document.getElementById('order-name').innerHTML = doc.data().name;
+                document.getElementById('order-carName').innerHTML = doc.data().carName;
                 document.getElementById('order-contact').innerHTML = doc.data().contact;
                 document.getElementById('order-address').innerHTML = doc.data().address;
                 document.getElementById('order-pinCode').innerHTML = doc.data().pinCode;
-                document.getElementById('order-amount').innerHTML = doc.data().amount/100;
-                document.getElementById('order-time').innerHTML = doc.data().timestamp.toDate().toLocaleTimeString();
-                document.getElementById('order-carName').innerHTML = doc.data().carName;
                 document.getElementById('order-carModel').innerHTML = doc.data().carModel;
                 document.getElementById('order-carNumber').innerHTML = doc.data().carNumber;
+                document.getElementById('order-amount').innerHTML = doc.data().amount / 100;
                 document.getElementById('order-transactionId').innerHTML = doc.data().transactionId;
+                document.getElementById('order-time').innerHTML = doc.data().timestamp.toDate().toLocaleTimeString();
             });
         })
         .catch(function (error) {
             console.log("Error getting documents: ", error);
         });
-
+    
+        var btn2 = document.getElementById("myBtn2");
+        btn2.onclick = function() {
+            openModal(userId);
+        }
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
@@ -98,3 +101,59 @@ function on(userId) {
         }
     }
 }
+
+
+
+function openModal(id) {
+    var modal = document.getElementById("assign-modal");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close-assignModal")[0];
+
+    var saveChanges = document.getElementsByClassName("close-saveChanges")[0];
+    // When the user clicks the button, open the modal 
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    var btn = document.getElementById('assigning-button');
+    btn.onclick = function() {
+        var nameAssigned = document.getElementById('assign-input').value;
+        console.log(nameAssigned);
+        db.collection("portal").doc(id).update({
+                assignTo: nameAssigned
+            })
+            .then(function () {
+                console.log("Document successfully written!");
+            })
+            .catch(function (error) {
+                console.error("Error writing document: ", error);
+            });
+        console.log("Assigned");
+        btn.innerHTML = "Success";
+        btn.className = "btn btn-success"
+    }
+    
+
+    saveChanges.onclick = function () {
+        modal.style.display = "none";
+        location.reload();
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+/*
+db.collection("portal").onSnapshot(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+        console.log(doc.id + " -> " + doc.data());
+    });
+});
+*/
